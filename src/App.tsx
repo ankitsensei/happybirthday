8b6f68b964e3c48e89b1a2163682917b64b96771
ankitsensei <main.ankitbhagat@gmail.com>
2024-09-13 23:42:57 +0530

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './home';
 
 const App: React.FC = () => {
+    const router = createBrowserRouter([
+        { path: '/home.txs', element: <Home /> },
+        {},
+    ]);
     return (
         <div className='flex flex-col w-full h-screen justify-center items-center'>
             <button className='text-3xl outline-none border-2 border-black px-5 py-2 rounded-lg hover:bg-black hover:text-white transition-all duration-500 ease-in-out transform hover:scale-110'>
-                surprise box
+                <a href='/home.txs'>Click me!</a>
             </button>
+            <RouterProvider router={router} />
         </div>
     );
 };
--- src/home.tsx
+++ src/home.tsx
@@ -3,7 +3,7 @@ import CakeImg from './assets/imgs/cake.png';
 const home: React.FC = () => {
     return (
         <div className='flex flex-col justify-center items-center w-full h-screen bg-mainbg bg-cover'>
-            <img src={CakeImg} alt='cake' className='w-[1000px] h-auto ' />
+            <img src={CakeImg} alt='cake' className='w-[600px] h-auto ' />
         </div>
     );
 };
