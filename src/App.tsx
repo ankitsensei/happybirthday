834d083e766ee8a6a8c273cbcb01cd1d893e4cc8
ankitsensei <main.ankitbhagat@gmail.com>
2024-09-13 08:14:31 +0530

    new project... happybirthday

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1 className='text-7xl text-red-600 font-semibold'>Hello Sir</h1>
+            <button>Nice</button>
+        </div>
+    );
+};
+
+export default App;
